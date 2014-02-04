f3dc276ec08f0bdd058aab32baf1b54db26009c0
jimiljojo <jimiljojo@gmail.com>
2014-02-03 20:11:07 -0500

    added some hardcoded view data for reference

--- homeowner/view/info.php
+++ homeowner/view/info.php
@@ -6,8 +6,43 @@
 
 
 ?>
-<h2>homeowner/view/info.php</h2>
+<style type="text/css">
+table.hoTable {width: 100%;}
+img.license {display: block;   margin-left: auto;   margin-right: auto;}
+img.licenseThumb {
+    height: 20px;
+    width: 30px;
+}</style>
+<link rel="stylesheet" type="text/css" href="info.css"> 
+<h4>Home Owner Information</h4>
 <hr>
-<ul>
-</ul>
+<h4>Status</h4>
+In Process (IP)
+<h4>Members</h4>
+<table class="hoTable">
+    <tr><th>Members</th><th>License</th></tr>
+    <tr>
+	<td>
+	    <dl>
+		<dt>Father<dt>
+		    <dd>John Doe</dd>
+		    <dd><img class="licenseThumb" src="img/license.jpg" alt="license image" /></dd>
+		<dt>Mother<dt>
+		    <dd>Jane Doe</dd>
+		    <dd><img class="licenseThumb" src="img/license.jpg" alt="license image" /></dd>
+		<dt>Daughter<dt>
+		    <dd>Janice Doe</dd>
+		<dt>Son<dt>
+		    <dd>Jack Doe</dd>
+	    </dl>
+	</td>
+	<td>
+	    <img class="license" src="img/license.jpg" alt="license image" />
+	</td>
+    </tr>
+</table>
 <hr>
+<h4>Sweat Equity Data</h4>
+
+<hr>
+<h4>Mortgage Data</h4>
