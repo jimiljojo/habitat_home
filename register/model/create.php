84c249b4efcff62a50a340cd54357f6263d7c940
jimiljojo@gmail.com <rassy2002>
2014-02-18 11:33:36 -0500

    changed the method of accessing objects

--- register/model/create.php
+++ register/model/create.php
@@ -10,24 +10,24 @@
 	global $msg;
 	global $dbio;
 	//Account class inculsion
-	include ('../class/account.php');
+	include ('../class/account->php');
 
 	//instantaited account object
 	Account $account = new Account();
 
-	$account.fname = $_GET['fname'];
-	$account.lname = $_GET['lname'];
-	$account.street1 = $_GET['street1'];
-	$account.street2 = $_GET['street2'];
-	$account.city = $_GET['city'];
-	$account.state = $_GET['state'];
-	$account.zip = $_GET['zip'];
-	$account.phone = $_GET['phone'];
-	$account.email = $_GET['email'];
-	$account.employer = $_GET['employer'];
-	$account.workPhone = $_GET['workPhone'];
-	$account.title = $_GET['title'];
-	$account.cellPhone = $_GET['cellPhone'];
+	$account->fname = $_GET['fname'];
+	$account->lname = $_GET['lname'];
+	$account->street1 = $_GET['street1'];
+	$account->street2 = $_GET['street2'];
+	$account->city = $_GET['city'];
+	$account->state = $_GET['state'];
+	$account->zip = $_GET['zip'];
+	$account->phone = $_GET['phone'];
+	$account->email = $_GET['email'];
+	$account->employer = $_GET['employer'];
+	$account->workPhone = $_GET['workPhone'];
+	$account->title = $_GET['title'];
+	$account->cellPhone = $_GET['cellPhone'];
 
 	$dbio->addAccount($account);
 
