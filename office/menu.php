122d3ed3282e2910c82a245c9fe332cb4c0e30b9
jimiljojo@gmail.com <rassy2002>
2014-03-02 12:59:56 -0500

    removing extra submnus from office

--- office/menu.php
+++ office/menu.php
@@ -9,10 +9,8 @@
 
 	$subLinks[] = new Link('Accounts','accounts');
 	$subLinks[] = new Link('Donations','donations');
-	$subLinks[] = new Link('Donors','donor');
 	$subLinks[] = new Link('Events','event');
 	$subLinks[] = new Link('Friends of Habitat','foh');
-	$subLinks[] = new Link('Home Owners','homeowner');
 	$subLinks[] = new Link('Interests','interests');
 	$subLinks[] = new Link('Organizations','orgs');
 	$subLinks[] = new Link('Projects','projects');
