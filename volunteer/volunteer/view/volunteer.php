80f753aa542f789c18bd5bd442a05e6746958cfe
averonski <averonski@gmail.com>
2014-03-05 02:16:54 -0800

    changed css

---
+++ volunteer/volunteer/view/volunteer.php
@@ -0,0 +1,43 @@
+<?php
+    //File: volunteer view
+	//Author: Brandon Willis; bmw5285
+?>
+<style>
+	dd.list
+		{
+		border-collapse:collapse;
+		border-bottom-style:dotted;
+		border-width: 1px;
+		border-color:lightgrey;
+		}
+	dd.list
+		{
+		padding:20px;
+		padding-left:60px;
+		}
+	dt.list
+		{
+		padding:5px;
+		padding-left:40px;
+		}
+	h4
+		{
+
+		}
+</style>
+
+<h4>Volunteer</h4>
+<hr>
+
+<dl>
+    <dt class="list"><a href="/habitat/index.php?dir=volunteer&sub=availability">Availability</a></dt>
+	<dd class="list">Shows a volunteer's availability</dd>
+    <dt class="list"><a href="/habitat/index.php?dir=volunteer&sub=consent">Consent</a></dt>
+	<dd class="list">Shows the consents the volunteer agreed to</dd>
+    <dt class="list"><a href="/habitat/index.php?dir=volunteer&sub=interests">Interests</a></dt>
+	<dd class="list">View and edit the volunteer work interests</dd>
+    <dt class="list"><a href="/habitat/index.php?dir=volunteer&sub=schedule">Schedule</a></dt>
+	<dd class="list">View and edit the volunteer schedule</dd>
+    <dt class="list"><a href="/habitat/index.php?dir=volunteer&sub=history">Work History</a></dt>
+	<dd class="list">View and print a history of the volunteer hours and jobs worked</dd>
+</dl>
