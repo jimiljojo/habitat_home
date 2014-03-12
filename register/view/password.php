67165de25abc36dd00165bf3f8c0f213b65cb516
Dhaval <dumehta103@live.com>
2014-03-12 18:08:59 -0400

    Register
    Javascript code to verify whether the passwords match each other or not
    and to check whether all fields in the form have been entered.

--- register/view/password.php
+++ register/view/password.php
@@ -61,12 +61,46 @@
 	    	<input type="checkbox" name="volunteer">Volunteer</label><br> </table></div>
 	    <br/> -->
 	    <h4 class="show" onclick="swap(this);">Username and Password</h4><div><table class="intTable">
-	    <tr><td><label><span class="mandatory">*</span>Username</td><td> <input type="text" name"username"></label></td></tr>
+	    <tr><td><label><span class="mandatory">*</span>Username</td><td> <input type="text" name"username" id="username"></label></td></tr>
 	    
-	    <tr><td><label><span class="mandatory">*</span>Password</label></td><td><input type="password" name="pw1"></td></tr><br>
-	    <tr><td><label><span class="mandatory">*</span>Confirm</label></td><td><input type="password" name="pw2"> </td><td>&nbsp<span style="color:lightgrey; font-size: 10pt;">re-type password</span></td></tr></table></div><br>
+	    <tr><td><label><span class="mandatory">*</span>Password</label></td><td><input type="password" name="pw1" id="pw1"></td></tr><br>
+	    <tr><td><label><span class="mandatory">*</span>Confirm</label></td><td><input type="password" name="pw2" id="pw2"> </td><td>&nbsp<span style="color:lightgrey; font-size: 10pt;">re-type password</span></td></tr></table></div><br>
 	    <br>
-	    <input class="btn btn-success" name="submit" type="submit" value="submit" >
+	    <script type="text/javascript">
+	    function check()
+            {
+            	if (document.getElementById('username').value==""
+                 || document.getElementById('username').value==undefined)
+                {
+                    alert ("Please Enter your prefered Username");
+                    return false;
+                }
+
+                if (document.getElementById('pw1').value==""
+                 || document.getElementById('pw1').value==undefined)
+                {
+                    alert ("Please Enter your prefered Password");
+                    return false;
+                }
+
+                if (document.getElementById('pw2').value==""
+                 || document.getElementById('pw2').value==undefined)
+                {
+                    alert ("Please Re-Type your Password");
+                    return false;
+                }
+
+                if (document.getElementById('pw1').value!=document.getElementById('pw2').value)
+                {
+                    alert ("Your Passwords don't match!");
+                    return false;
+                }
+
+                return true;
+            }
+         </script>
+	    <input class="btn btn-success" name="submit" type="submit" value="submit" onclick="return check();" >
+
 	    <br>
 	</form>
 	<br>
