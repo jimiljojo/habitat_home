29f6f05ff2314d4b6d1d13e4cd911e89e97a6f6a
logan <logan@logan-Aspire-4530>
2014-03-13 04:54:37 -0400

    Coded in php with minor changes to it & calculated time worked.

--- volunteer/view/history.php
+++ volunteer/view/history.php
@@ -1,12 +1,45 @@
+<h2>volunteer/view/history.php</h2>
+<hr>
+
 <?php
 
 	// TITLE: Volunteer Work History View
 	// FILE: volunteer/view/history.php
-	// AUTHOR: AUTOGEN
+	// AUTHOR: Logan Gurreri
 
+        $workHistory = array('1', '2', '3', '4', '5');
+        global $workHistory2;
+        
+        $association = array('Penn State Build', 'Charity Event', 'Dinner', 'Meeting', 'Fundraiser');
+        $date = array('20140305', '20140309', '20140313', '20140315','20140401');
+        $start = array ('1100', '0800', '0800', '0700','0700');
+        $end = array ('1230', '0830', '0900', '0830', '0800');
+        $auth = array ('No', 'No', 'No', 'Yes', 'Yes');
+        
+        $month;
+        $day;
+        $year;
+        
+        $startHours;
+        $endHours;
+        $startMins;
+        $endMins;
+        
+        $timeOfDay;
+        $timeOfDay2;
+        
+        global $totalHours;
+        global $totalEndMin;
+        global $totalStartMin;
+        global $totalMin;
+        
+        $totalHours = 0;
+        $totalEndMin = 0;
+        $totalStartMin = 0;
+        $totalMin = 0;
+         
 ?>
-<h2>volunteer/view/history.php</h2>
-<hr>
+
 <style>
     table,th,td
     {
@@ -15,6 +48,8 @@
         padding: 5px;
     }
 </style>
+
+<form>
 <table>
     <tr>
         <th>Event/Project</th>
@@ -23,7 +58,76 @@
         <th>End Time</th>
         <th>Authorized</th>
     </tr>
+    <?php
+        global $col;
+                
+        $col = count($workHistory);
+        
+        for ($i = 0; $i < $col;)
+        {
+            $workHistory2 = $workHistory[$i];
+            
+            $month = substr($date[$i],4,2);
+            $day = substr($date[$i],6,8);
+            $year = substr($date[$i],0,4);
+
+            $startHours = substr($start[$i],0,2);
+            $startMins = substr($start[$i],2,5);
+            
+            $endHours = substr($end[$i],0,2);
+            $endMins = substr($end[$i],2,4);
+            
+            $timeOfDay = ($startHours > 11) ? 'PM' : 'AM';
+            $timeOfDay2 = ($endHours > 11) ? 'PM' : 'AM';
+            
+            $totalHours = $totalHours+($endHours - $startHours);
+            $totalEndMin = $totalEndMin+($endMins);
+            $totalStartMin = $totalStartMin+($startMins);
+        
+            
+            if ($startHours > 12) {$startHours = strval($startHours-12);}
+            if ($endHours > 12) {$endHours = strval($endHours-12);}
+            
+            echo '<tr>';
+                echo '<td>' . $association[$i] . '</td>';
+                echo '<td>' . $month . '/' . $day . '/' . $year . '</td>';
+                echo '<td>' . $startHours . ':' . $startMins . ' ' . $timeOfDay . '</td>';
+                echo '<td>' . $endHours . ':' . $endMins . ' ' . $timeOfDay2 . '</td>';
+                echo '<td>' . $auth[$i] . '</td>';
+            echo '</tr>';
+            
+            $i++;
+            
+        }
+        
+        $totalMin = $totalMin+($totalEndMin-$totalStartMin);
+        
+        while ($totalMin >= 60)
+        {
+            $totalMin = $totalMin - 60;
+            $totalHours = $totalHours + 1;                 
+        }
+        
+        while ($totalMin < 0)
+        {
+            $totalMin = $totalMin + 60;
+            $totalHours = $totalHours - 1;
+        }
+    ?>
+   
 </table>
-<h5>Total Hours Worked: </h5>
+    <h5>Total Time Worked:
+    <?php
+    if ($totalMin == "0")
+    {
+        echo $totalHours . ' hours.';
+    }
+    else
+    {
+        echo $totalHours . ' hours ' . $totalMin . ' minutes.';
+    }
+    ?></h5>
+    
+</form>
 <hr>
 
