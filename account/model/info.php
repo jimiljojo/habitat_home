567dde37d08f59d3e25b9ce36e6a92c37143e9b1
jimiljojo@gmail.com <rassy2002>
2014-03-12 22:15:40 -0400

    the info view now pulls title, fname and lname from the database -- working on preferences, address and contact information

--- account/model/info.php
+++ account/model/info.php
@@ -3,13 +3,21 @@
 	// TITLE: Account Personal Info Model
 	// FILE: account/model/info.php
 	// AUTHOR: AUTOGEN
+	
+	$accountid = 5;
+	$account = new Account();
+	$account= $dbio->readAccount($accountid);
 
 
-	function search() {}
-	function create() {}
-	function read() {}
-	function update() {}
-	function delete() {}
-	function list() {}
+	//read person from account
+	$person = new Person();
+	$person = $dbio->readPerson($account->getPerson());
+
+	// function search() {}
+	// function create() {}
+	// function read() {}
+	// function update() {}
+	// function delete() {}
+	// function list() {}
 
 ?>
