016b4f7e0fb5a9ec66c7a6048f6b32cce4b76c5a
Dhaval <dumehta103@live.com>
2014-03-14 03:19:45 -0400

    Created a new Login Page
    Added Model, View, Controller (under construction)

---
+++ login/index.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+
+<div class="navbar navbar-inverse">
+  <div class="navbar-inner">
+    <a class="brand">Habitat for Humanity Login Page:</a>
+  </div>
+</div>
+
+<div style="padding-left: 10px;">
+<?php
+	
+	//require_once('/class/item.php');
+	
+	//require_once ('/model/dbio_register.php');
+	//$dbio= new DBIO();
+
+	$act = (isset($_GET['act'])) ? $_GET['act'] : '';
+	$msg = (isset($_GET['msg'])) ? $_GET['msg'] : '';
+	include 'controller/login.php';
+
+	
+?>
+</div>
