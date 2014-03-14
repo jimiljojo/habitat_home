f692ddebad4495900787d1c53153951856245237
jimiljojo@gmail.com <rassy2002>
2014-03-13 20:31:44 -0400

    working on accountss in office

--- office/model/accounts.php
+++ office/model/accounts.php
@@ -3,13 +3,27 @@
 	// TITLE: Office Accounts Model
 	// FILE: office/model/accounts.php
 	// AUTHOR: AUTOGEN
-
+	$accounts = array();
+	$accounts = $dbio->readAccounts();
+	
 
 	function search() {}
 	function create() {}
-	function read() {}
+	function read($accounts) {
+		echo '<table style="width:500px"><th>Account ID  </th><th>Username</th><th>Password</th><th>Date</th><th>Status</th><thIsOffice></th><th>isAdmin</th><th>isVol</th><th>Person ID</th>';
+		foreach ($accounts as $account) {
+			echo '<tr><td>' . $account->getAccount_id() . '</td>';
+			echo '<td>' . $account->getUsername() . '</td>';
+			echo '<td>' . $account->getPassword() . '</td>';
+			echo '<td>' . $account->getDate() . '</td>';
+			echo '<td>' . $account->getStatus() . '</td>';
+			echo '<td>' . $account->getIsOffice() . '</td>';
+			echo '<td>' . $account->getIsVolunteer() . '</td>';
+			echo '<td>' . $account->getPerson() .'</td></tr>';
+		}
+		echo '</table>';
+	}
 	function update() {}
 	function delete() {}
-	function list() {}
 
 ?>
