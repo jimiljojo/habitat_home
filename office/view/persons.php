ad7236f984e562335f5b57032b3b5f2837294d3b
THE_ENTITY <THE_ENTITY@192.168.2.4>
2014-03-14 22:56:10 -0400

    added Persons to office section

---
+++ office/view/persons.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+	// TITLE: Office Persons View
+	// FILE: office/view/persons.php
+	// AUTHOR: jmh5819
+
+
+?>
