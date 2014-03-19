480c5803fc65948321010885157536033c2ffa6a
rwg5215 <rwg5215@psu.edu>
2014-03-19 13:26:56 -0400

    edited volunteer model

--- office/model/volunteer.php
+++ office/model/volunteer.php
@@ -4,7 +4,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- * Author: rwg5215
+ * Author: Roman Galysh
  */
 
 
@@ -17,7 +17,6 @@ public function searchVolunteerBy($value1, $value2) {}
 public function createVolunteer($title,$gender,$marital_status,$fname,$lname,$addr,$city,$state,$zip,$phone,$dob);
 
 public function listVolunteer();
-
 
 
 
