efe107632fc1ced3951692c408a9b8807991e8cf
rwg5215 <rwg5215@psu.edu>
2014-03-19 18:10:54 -0400

    edit volunteer view

--- office/view/volunteer.php
+++ office/view/volunteer.php
@@ -14,7 +14,45 @@
     </head>    
     <body>
     <h4>Volunteer Search</h4>   
-        
+    <p>
+        <hr>
+<button type="Submit" onclick="window.location='index.php?dir=office&sub=volunteer&act=listVolunteer';">View All</button><form method="GET" action="index.php">
+    <input type="hidden" name="dir" value="office" >
+    <input type="hidden" name="sub" value="volunteer" >
+    <input type="hidden" name="act" value="" >
+    <input name="search" type="submit" value="Search By">
+    <select id="searchBy" name="searchBy" onchange="searchByHandler();">
+	<option value="name" selected="selected">Name</option>
+	<option value="organization">Organization</option>
+	<option value="street">Street</option>
+	<option value="city">City</option>
+    </select>
+    <input id="input1" name="input1" placeholder="first name" type="text">
+    <input id="input2" name="input2" placeholder="last name" type="text">
+</form>
+<script>
+    var msg = "This part is under construction"
+</script>
+
+<button type="Submit" onclick="alert(msg);">Create New</button><!--
+<button type="Submit" onclick="window.location=\'index.php?dir=&act=newVolunteer\';">Create New</button>
+-->
+<hr>
+<div>
+    Here you can search for a volunteer by:
+    <ul>
+        <li>Volunteer Name</li>
+        <li>Organization</li>
+        <li>Street Address</li>
+        <li>City</li>
+    </ul>
+    Only an administrator will be able to create a new volunteer
+    <br>
+       </div>
+    </div>
+    </body>	<footer>
+	2014 <span class="habitatBlue">|</span> York Habitat for Humanity
+	</footer>
     </body>
 </html>
 
