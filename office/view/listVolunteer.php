4ed301b4b8a9d06418fcc2113b62630ececa2028
rwg5215 <rwg5215@psu.edu>
2014-03-26 13:55:11 -0400

    edit file listVolunteer

--- office/view/listVolunteer.php
+++ office/view/listVolunteer.php
@@ -8,30 +8,73 @@
  * Author: rwg5215
  */
 ?>
+<?php
 
-<div id="content">
-	    <h4></h4>
-<hr>
+	$fname = array('Mary', 'Patricia', 'Linda', 'Barbara', 'Elizabeth', 'Jennifer', 'Maria', 'Susan', 'Margareet', 'Dorothy', 'James','John','Robert','Michael','William','David','Richard','Charles','Joseph','Thomas');
+    $lname = array('Smith','Johnson','Williams','Brown','Jones','Miller','Davis','Garcia','Rodriguez','Wilson');
+    $sname = array('Second', 'Third', 'First','Fourth','Park','Fifth','Main','Sixth','Oak','Seventh');
+    $stype = array('St', 'Ave', 'Rd', 'Way', 'Ln');
+    $states = array('PA', 'MD', 'NY', 'NJ', 'OH', 'VA', 'IL', 'WV', 'IN', 'TX');
+    $cities = array('East St. Louis','Camden','Flint','West Memphis','Saginaw','Detroit','Atlantic City','St. Louis','Newburg','Inkster');
+	
+?>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<style>
+			table {border-collapse: collapse;}
+			tr:nth-child(2n) {background-color: lavender;}
+			tr:hover {background-color: gold;}
+			td {padding: 0px 10px;}
+		</style>
+		<script>
+			function retreive(n) {
+				var dir = "&dir=" + document.getElementById("dir").value;
+				var sub = "&sub=" + document.getElementById("sub").value;
+				var act = "&act=" + document.getElementById("act").value;
+				var url = "index.php?id=" + n + dir + sub + act;
+				alert(url);
+				// window.location = url;
+			}
+		</script>
+	</head>
+	<body>
+		<input id="dir" type="hidden" value="A">
+		<input id="sub" type="hidden" value="B">
+		<input id="act" type="hidden" value="retrieve">
+		<table>
+		<?php
+			for ($id=0; $id<15; $id++) {
+				
+				// $id = object->getId();	
+				$name = $lname[rand(0, sizeof($lname) - 1)] . ', ' . $fname[rand(0, sizeof($fname) - 1)];
+				$month = rand(1,12);
+				$day = rand(1,30);
+				$year =  rand (19,20) . rand(0,9) . rand(0,9);
+				$street =  rand(0, 99) . ' ' . $sname[rand(0, sizeof($sname) - 1)] . ' ' . $stype[rand(0, sizeof($stype) - 1)];
+				$city = $cities[rand(0, sizeof($cities) - 1)];
+				$state = $states[rand (0, sizeof($states) - 1)];
+				$zip = rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9) . rand(0,9);
+				
+				echo '<tr onclick="retreive(' . $vid . ');">'; // <tr onclick="retreive(n);">
+				
+				echo '<td>' . $name .'</td>';
+				echo '<td>Dr.</td>';
+				echo '<td class="right">' . $month . '-' . $day . '-' . $year . '</td>';
+				echo '<td class="right">' . $street . '</td>';
+				echo '<td>' . $city . '</td>';
+				echo '<td class="right">' . $state . '</td>';
+				echo '<td>' . $zip . '</td>';
+				
+				echo '</tr>';
+			}
+		?>
+		</table>
+		<br>
+		<hr>
+		<br>
+		<div id="notes"></div>
 
-<br/>
-<table class="nameTable">
-    <tr>
-	<th>Name</th>
-	<th>Title</th>
-	
-	<th>DOB</th>
-	
-	<th>Street</th>
-	<th>City</th>
-	<th>State</th>
-	<th>Zip</th>
-    </tr>
-<tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=0">Smith, Patricia</a></td><td>Dr.</td><td class="right">10-14-2035</td><td class="right">79 Seventh Rd</td><td>Detroit</td><td class="right">PA</td><td>15649</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=1">Wilson, Barbara</a></td><td>Dr.</td><td class="right">8-26-1906</td><td class="right">4 Main Ave</td><td>Detroit</td><td class="right">IN</td><td>92900</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=2">Brown, Michael</a></td><td>Dr.</td><td class="right">5-29-1945</td><td class="right">5 Park Way</td><td>Atlantic City</td><td class="right">MD</td><td>15031</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=3">Wilson, Michael</a></td><td>Dr.</td><td class="right">4-17-2097</td><td class="right">24 Oak Way</td><td>Saginaw</td><td class="right">WV</td><td>75149</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=4">Smith, Charles</a></td><td>Dr.</td><td class="right">5-17-2082</td><td class="right">56 Seventh Rd</td><td>Camden</td><td class="right">PA</td><td>82945</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=5">Jones, Elizabeth</a></td><td>Dr.</td><td class="right">6-9-1920</td><td class="right">96 Seventh Way</td><td>Saginaw</td><td class="right">PA</td><td>14108</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=6">Garcia, Thomas</a></td><td>Dr.</td><td class="right">8-29-2054</td><td class="right">66 Fifth Ave</td><td>Inkster</td><td class="right">OH</td><td>74998</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=7">Williams, Margareet</a></td><td>Dr.</td><td class="right">2-9-1910</td><td class="right">86 First Ave</td><td>West Memphis</td><td class="right">NJ</td><td>31037</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=8">Smith, Charles</a></td><td>Dr.</td><td class="right">4-13-2086</td><td class="right">46 Fourth Ave</td><td>Inkster</td><td class="right">NY</td><td>28569</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=9">Rodriguez, Mary</a></td><td>Dr.</td><td class="right">1-27-2021</td><td class="right">23 Main Rd</td><td>West Memphis</td><td class="right">WV</td><td>70763</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=10">Johnson, Barbara</a></td><td>Dr.</td><td class="right">3-23-2050</td><td class="right">57 Oak Ave</td><td>West Memphis</td><td class="right">NJ</td><td>03204</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=11">Johnson, Mary</a></td><td>Dr.</td><td class="right">9-8-1937</td><td class="right">58 Second Rd</td><td>Atlantic City</td><td class="right">WV</td><td>10932</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=12">Davis, Thomas</a></td><td>Dr.</td><td class="right">10-24-2061</td><td class="right">93 Seventh St</td><td>Flint</td><td class="right">NY</td><td>27324</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=13">Miller, James</a></td><td>Dr.</td><td class="right">10-9-1987</td><td class="right">79 Main Ln</td><td>Newburg</td><td class="right">VA</td><td>21229</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=14">Smith, William</a></td><td>Dr.</td><td class="right">7-8-2054</td><td class="right">95 Sixth Way</td><td>Atlantic City</td><td class="right">PA</td><td>00658</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=15">Rodriguez, Michael</a></td><td>Dr.</td><td class="right">10-20-1945</td><td class="right">31 Second Way</td><td>Saginaw</td><td class="right">NJ</td><td>93479</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=16">Davis, Margareet</a></td><td>Dr.</td><td class="right">6-4-1928</td><td class="right">4 First Ln</td><td>Camden</td><td class="right">IN</td><td>40717</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=17">Jones, John</a></td><td>Dr.</td><td class="right">3-30-2017</td><td class="right">33 Fifth Way</td><td>Atlantic City</td><td class="right">TX</td><td>56590</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=18">Davis, Maria</a></td><td>Dr.</td><td class="right">4-16-1954</td><td class="right">48 Park Ln</td><td>Detroit</td><td class="right">MD</td><td>92554</td></tr><tr><td><a href="index.php?dir=office&sub=volunteer&act=listVolunteer&vid=19">Miller, Margareet</a></td><td>Dr.</td><td class="right">8-11-2011</td><td class="right">48 Second Way</td><td>East St. Louis</td><td class="right">IL</td><td>51384</td></tr></table>
-<br/>
-<center><input type="button"  class="btn btn-primary btn-sm" onclick="history.back();" value="Back"></center>
-<!--<button>Search</button>-->
-<hr>        </div>
-    </div>
     </body>	<footer>
 	2014 <span class="habitatBlue">|</span> York Habitat for Humanity
 	</footer>
