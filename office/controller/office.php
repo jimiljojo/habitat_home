c874b1aeb5f60412df515a9e809774b331e9547f
rwg5215 <rwg5215@psu.edu>
2014-03-26 19:50:57 -0400

    create/edit office controller

--- office/controller/office.php
+++ office/controller/office.php
@@ -2,25 +2,49 @@
 
 	// TITLE: Office Office Controller
 	// FILE: office/controller/office.php
-	// AUTHOR: AUTOGEN
+	// AUTHOR: rwg5215
 
 
 	switch ($act) {
 
 		case 'search':
-			// CODE HERE
+                    $parameter = $_GET['param'];
+                    $value1 = $_GET['i1'];
+                    $value2 = $_GET['i2'];
+                  
+                    
+                    include $dir . '/model/' . $sub . '.php';
+                    $employee = listEmployeeBy($value1, $value2);
+                    $page = $dir . '/view/list' . ucfirst($sub) . '.php';
+ 
 			break;
 
 		case 'create':
-			// CODE HERE
+                    $parameter = $_GET['param'];
+                    $title = $_GET['title'];
+                    //$gender = $_GET['gender'];
+                    $fname = $_GET['fn'];
+                    $lname = $_GET['ln'];
+                    $addr = $_GET['addr'];
+                    $city = $_GET['city'];
+                    $state = $_GET['state'];
+                    $zip = $_GET['zip'];
+                    $phone = $_GET['pn'];
+                    $dob = $_GET['dob'];
+                    
+                    include $dir . '/model/' . $sub . '.php';
+                    $volunteers = createEmployeeBy($parameter);
+                    $page = $dir . '/view/create' . ucfirst($sub) . '.php';
+                    
 			break;
 
 		case 'read':
 			// CODE HERE
 			break;
 
-		case 'update':
-			// CODE HERE
+		case 'edit':
+			include $dir . '/model/' . $sub . '.php';
+                        $page = $dir . '/view/edit' . ucfirst($sub) . '.php';
 			break;
 
 		case 'delete':
@@ -28,7 +52,9 @@
 			break;
 
 		case 'list':
-			// CODE HERE
+                    include $dir . '/model/' . $sub . '.php';
+                    $volunteers = listEmployee();
+                    $page = $dir . '/view/list' . ucfirst($sub) . '.php';
 			break;
 
 		default:
