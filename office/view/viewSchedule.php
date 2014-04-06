6feb1e2cb13228070b2cac09b23f331164c5606e
averonski <averonski@gmail.com>
2014-04-06 05:25:37 -0400

    Create viewSchedule.php

---
+++ office/view/viewSchedule.php
@@ -0,0 +1,11 @@
+<?php
+  //Author: bmw5285; copied from j*p*
+  
+  echo '<center><input type="button"  class="btn btn-primary btn-sm" onclick="history.back();" value="Back"></center>';
+  echo "<br><br>";
+  
+if($_GET['act'] == "viewInterestType")
+  {
+  	viewInterestType();
+  }
+?>
