fac420f6003801b999614efd0fb83cc24355942f
averonski <averonski@gmail.com>
2014-04-06 05:26:37 -0400

    Update schedules.php

--- office/controller/schedules.php
+++ office/controller/schedules.php
@@ -38,4 +38,73 @@
 
 	}// end switch
 
+?><?php
+
+	// TITLE: Office Interests Controller
+	// FILE: office/controller/interests.php
+	// AUTHOR: AUTOGEN
+
+
+	switch ($act) {
+
+		case 'listSchedule':
+			include 'office/model/schedules.php';
+			$page = $dir . '/view/listSchedule.php';
+			break;
+			
+		case 'listScheduleSlot':
+			include 'office/model/schedules.php';
+			$page = $dir . '/view/listSchedule.php';
+			break;
+
+		case 'createSchedule':
+			include 'office/model/schedules.php';
+			$page = $dir . '/view/createSchedule.php';
+			break;
+                
+		case 'createScheduleSlot':
+			include 'office/model/schedules.php';
+			if(!isset($_GET['id']))
+			{
+				$page = $dir . '/view/' . (($sub) ? $sub : $dir) . '.php'; echo "shit son";
+			}
+			else
+			{
+				$page = $dir . '/view/createSchedule.php';
+			}
+			//$page = $dir . '/view/createSchedule.php';
+			break;
+			
+		case 'readScheduleByEvent':
+			include 'office/model/schedules.php';
+			$page = $dir . '/view/listSchedule.php';
+			break;
+			
+		case 'readScheduleByName':
+			include 'office/model/schedules.php';
+			$page = $dir . '/view/listSchedule.php';
+			break;
+
+		case 'updateInterests':
+			include 'office/model/schedules.php';
+			$page = $dir . '/view/listInterests.php';
+			break;
+          
+		case 'updateInterestTypes':
+			include 'office/model/schedules.php';
+			$page = $dir . '/view/listInterests.php';
+			break;
+			
+		case 'viewInterestType':
+			include 'office/model/schedules.php';
+			$page = $dir . '/view/viewInterests.php';
+			break;
+	
+		default:
+			$page = $dir . '/view/' . (($sub) ? $sub : $dir) . '.php';
+			break;
+
+	}// end switch
+
 ?>
+
