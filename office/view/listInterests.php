13cecbdcaf20f071363393181fed48f4681db3a4
averonski <averonski@gmail.com>
2014-04-06 05:19:55 -0400

    Update listInterests.php

--- office/view/listInterests.php
+++ office/view/listInterests.php
@@ -1,5 +1,22 @@
+<script>
+	function retreive(n)
+	{
+		var dir = "&dir=" + document.getElementById("dir").value;
+		var sub = "&sub=" + document.getElementById("sub").value;
+		var act = "&act=" + document.getElementById("act").value;
+		//var url = "index.php?=" + dir + sub + act + "&id=" + n;
+		//alert(url);
+		window.location.assign("index.php?=" + dir + sub + act + "&id=" + n)
+		// window.location = url;
+	}
+</script>
 <?php
   //Author: bmw5285; copied from j*p*
+	/*
+	echo '<input name="dir" type="hidden" value="<?php echo $dir; ?>">';
+	echo '<input name="sub" type="hidden" value="<?php echo $sub; ?>" >';
+	echo '<input id="act" type="hidden" value="retrieve">';
+	*/
   
   echo '<center><input type="button"  class="btn btn-primary btn-sm" onclick="history.back();" value="Back"></center>';
   echo "<br><br>";
@@ -11,5 +28,13 @@
   elseif($_GET['act'] == "listInterestTypes")
   {
   	listInterestTypes();
+  }
+  elseif($_GET['act'] == "readInterest")
+  {
+  	readInterest();
+  }
+  elseif($_GET['act'] == "readInterestType")
+  {
+  	readInterestType();
   }
 ?>
