501c5b18e953faeb7d1165ce48996b0b33e8092f
averonski <averonski@gmail.com>
2014-04-18 04:46:56 -0400

    Update createSchedule.php
    changed to integrate with event

--- office/view/createSchedule.php
+++ office/view/createSchedule.php
@@ -1,3 +1,7 @@
+<?php
+//Author: bmw5285
+?>
+
 <script>
 	function reload()
 	{
@@ -6,115 +10,73 @@
 	}
 </script>
 
-<?php
-//Author: bmw5285; copied from j*p*
+<style>
 
-echo '<center><input type="button"  class="btn btn-primary btn-sm" onclick="history.back();" value="Back"></center>';
-echo "<br><br>";
+	.bold {font-weight: bold;}
+	.note {font-size: 10pt; color: grey;}
+	.mandatory {color: crimson;}	
 
-if($_GET['act'] == "createSchedule")
-{
-	echo '<form action="" method="post">';
-		echo '<select id="eventSchedule" name="event">';
-			echo '<option value="" disabled selected>-Select Event-</option>';
-				$events = $dbio->readAllEvent();
-				foreach ($events as &$event)
-				{
-					$eventName = $event->getTitle();
-					$eventId = $event->getEvent_id();
-					echo "<option value = '{$eventId}' name = '{$eventName}'>{$eventName}</option>";
-				}
-		echo "</select>";
-		echo "Time Start: <input type='text' name='timeStart'><br>";
-		echo "Time End: <input type='text' name='TimeEnd'>";
-		echo "Description: <input type='text' name='description'>";
-		echo '<select id="interest" name="interest">';
-			echo '<option value="" disabled selected>-Select Interest-</option>';
-				$interests = $dbio->listInterests();
-				foreach ($interests as &$interest)
-				{
-					$interestName = $interest->getTitle();
-					$interestId = $interest->getId();
-					echo "<option value = '{$interestId}' name = '{$interestName}'>{$interestName}</option>";
-				}
-		echo "</select>";
-		echo "<input type='submit' name='weOnTheMoon' value='Submit'>";
-	echo "</form>";
-	if (isset($_POST['weOnTheMoon']))
+	input
 	{
-		$timeStart = $_POST['timeStart'];
-		$timeEnd = $_POST['TimeEnd'];
-		$description = $_POST['description'];
-		$eventId = $_POST['event'];
-		$interestId = $_POST['interest'];
-		if (empty($eventId) || empty($timeStart) || empty($timeEnd) || empty($description) || empty($interestId))
-			{
-				echo "I am error";
-				echo "<br>";
-				echo $timeStart;
-				echo "<br>";
-				echo $timeEnd;
-				echo "<br>";
-				echo $description;
-				echo "<br>";
-				echo $eventId;
-				echo "<br>";
-				echo $interestId;
-			}
-			else
-			{
-				createSchedule($timeStart, $timeEnd, $eventId, $description, $interestId);
-			}
+		max-width:50%;
 	}
-}
-
-
-
-if($_GET['act'] == "createScheduleSlot")
-{
-echo '<div id="node-id">';
-	echo "<form action='' method='post'>";
-		echo '<select id="searchby" name="schedule">';
-			echo '<option disabled selected>-Select Schedule-</option>';
-				
-				$schedules = $dbio->readSchedule($_GET['id']);
-				foreach ($schedules as &$schedule)
-				{
-					$scheduleName = $schedule->getDescription();
-					$scheduleId = $schedule->getScheduleId();
-					echo "<option value = '{$scheduleId}' name = '{$scheduleName}'>{$scheduleName}</option>";
-				}
-		echo "</select>";
-		echo '<select id="searchby" name="person">';
-			echo '<option disabled selected>-Select Person-</option>';
-				$persons = $dbio->listPersons();
-				foreach($persons as &$person)
-				{
-					$personId = $person->getPerson_id();
-					$personFName = $person->getFirst_name();
-					$peronsLName = $person->getLast_name();
-					echo "<option value = '{$personId}' name = '{$personFName}, {$peronsLName}'>{$personFName}, {$peronsLName}</option>";
-				}
-		echo "<input type='submit' name='weOnTheMoon' value='Submit'>";
-	echo "</form>";
-echo '</div>';
 	
-	if (isset($_POST['weOnTheMoon']))
-	{
-		if (isset($_POST['schedule']) && isset($_POST['person']))
-		{
-			$schedule = $_POST['schedule'];
-			$person = $_POST['person'];
-			if (empty($schedule) || empty($person))
-			{
-				echo "I am error";
-			}
-			else
-			{
-				//echo "{$schedule}, {$person}";
-				createScheduleSlot($person, $schedule);
-			}
-		}
+    td
+    {
+        padding-left: 10px;
+        padding-bottom: 10px;
+        
+    }
+
+    div.show {display: block;}
+	div.hide {display: none;}
+	h4+div{border: 1px solid black;
 	}
-}
-?>
+	
+
+</style>
+
+<center><input type="button"  class="btn btn-primary btn-sm" onclick="history.back();" value="cancel"></center>
+<br><br>
+
+<h2 class="bold">Create Schedule</h2>
+	<form action="index.php" method="GET">
+		<input name="dir" id="dir" type="hidden" value="<?php echo $dir; ?>" >
+		<input name="sub" id="sub" type="hidden" value="<?php echo $sub; ?>" >
+		<input name="act" id="act" type="hidden" value="createSchedule" >
+		
+		<input type="hidden" name="eventId" value=<?php echo $_GET['eventId']; ?> >
+		<table cellspacing="10" class="intTable">
+			<tr>
+				<td>Description:<span class="mandatory">*</span></td>
+				<td><input type="text" name="description"></td>
+			</tr>
+			
+			<tr>	
+				<td>Time Start: <span class="mandatory">*</span></td>
+				<td><input type="text" name="timeStart"><label>HH:MM:SS</label></td>
+			</tr>
+			
+			<tr>
+				<td>Time End: <span class="mandatory">*</span></td>
+				<td><input type="text" name="timeEnd"><label>HH:MM:SS</label></td>
+			</tr>
+			
+			<tr>
+				<td>Interest: <span class="mandatory">*</span></td>
+				<td><select id="interest" name="interest">
+					<option value="null">-Select Interest-</option>
+					<?php $interests = getInterests();
+					foreach ($interests as $interest) {
+						?> <option value="<?php echo $interest->getId(); ?>" name="interest"><?php echo $interest->getTitle(); ?></option>
+					<?php } ?> </td>
+			</tr>
+			
+			<tr>
+				<td>Max Number of People: <span class="mandatory">*</span></td>
+				<td><input type="text" name="maxNumPeople"></td>
+			</tr>
+		</table> 
+
+		<input type="submit" value="create"> </form>	
+	</div>
