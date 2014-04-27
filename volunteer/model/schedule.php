7ca7e2116fcba55d05cb2cdae1fef4e530eaa49b
averonski <averonski@gmail.com>
2014-04-27 03:06:28 -0400

    Update schedule.php

--- volunteer/model/schedule.php
+++ volunteer/model/schedule.php
@@ -5,11 +5,40 @@
 	// AUTHOR: AUTOGEN
 
 
-	function search() {}
-	function create() {}
-	function read() {}
-	function update() {}
-	function delete() {}
-	function list() {}
+	//function search() {}
+	//function create() {}
+	//function read() {}
+	//function update() {}
+	//function delete() {}
+	//function list() {}
 
+	function readPersonEvents($personId) {
+		global $dbio;
+		$personSchedules = $dbio->readScheduleByName($personId);
+		return $personSchedules;
+	}
+	
+	function readEventsByInterest($interestIds) {
+		global $dbio;
+		$events = $dbio->readEventByInterest($interestIds);
+		return $events;
+	}
+	
+	function readInterestByPerson($personId) {
+		global $dbio;
+		$interestIds = $dbio->readInterestByPerson($personId);
+		foreach ($interestIds as $interestId) {
+			$interest = $interestId->getInterest();
+			$interests[] = $interest;
+		}
+		$interestIds = implode(',',$interests);
+		return $interestIds;
+	}
+	
+	function readEventAll() {
+		global $dbio;
+		$events = $dbio->readEventAll();
+		return $events;
+	}
+	
 ?>
